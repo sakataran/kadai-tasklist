796d12d813aa903152237099d980642ca5d632af
ran.sakata <sakataran@outlook.jp>
2021-06-03 13:27:21 +0000

    user login

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -2,18 +2,25 @@
     <nav class="navbar  navbar-expand-sm navbar-light bg-light">
         <a class="navbar-brand mb-0 h1" href="/">
         <i class="fas fa-tasks mr-2"></i>Tasklist</a>
-            
+
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
         <div class="collapse navbar-collapse" id="nav-bar">
             <ul class="navbar-nav mr-auto"></ul>
             <ul class="navbar-nav">
-                {{-- ゲスト時 --}}
-                {{-- ユーザ登録ページへのリンク --}}
-                <li>{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
-                {{-- ログインページへのリンク --}}
-                <li><a href="#">Login</a></li>
-                
-                {{-- ログイン時 --}}
-                <li class="nav-item">{!! link_to_route('tasks.create', 'Add', [], ['class' => 'nav-link']) !!}</li>
+                @if (Auth::check()) {{-- ログイン時 --}}
+                    {{-- 新規タスクへのリンク --}}
+                    <li class="nav-item">{!! link_to_route('tasks.create', 'Add', [], ['class' => 'nav-link']) !!}</li>
+                    {{-- ログアウトへのリンク --}}
+                    <li class="nav-item">{!! link_to_route('logout.get', 'Logout', [], ['class' => 'nav-link']) !!}</li>
+                @else {{-- ゲスト時 --}}
+                    {{-- ユーザ登録ページへのリンク --}}
+                    <li class="nav-item">{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
+                    {{-- ログインページへのリンク --}}
+                    <li class="nav-item">{!! link_to_route('login', 'Login', [], ['class' => 'nav-link']) !!}</li>
+                @endif
             </ul>
         </div>
     </nav>
