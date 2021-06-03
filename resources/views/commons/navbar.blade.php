5e08df22240642ef77b7d95576bbea25580bdbbc
ran.sakata <sakataran@outlook.jp>
2021-06-03 10:12:19 +0000

    user registration

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -6,6 +6,13 @@
         <div class="collapse navbar-collapse" id="nav-bar">
             <ul class="navbar-nav mr-auto"></ul>
             <ul class="navbar-nav">
+                {{-- ゲスト時 --}}
+                {{-- ユーザ登録ページへのリンク --}}
+                <li>{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
+                {{-- ログインページへのリンク --}}
+                <li><a href="#">Login</a></li>
+                
+                {{-- ログイン時 --}}
                 <li class="nav-item">{!! link_to_route('tasks.create', 'Add', [], ['class' => 'nav-link']) !!}</li>
             </ul>
         </div>
